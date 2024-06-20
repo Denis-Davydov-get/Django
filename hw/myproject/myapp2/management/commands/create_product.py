018fc6fb565f5b3dbe13e097d9b8fb89151725d6
Denis-Davydov-get <slim2001.88@mail.ru>
2024-06-20 10:23:08 +0300

    edit commands for product

--- hw/myproject/myapp2/management/commands/create_product.py
+++ hw/myproject/myapp2/management/commands/create_product.py
@@ -1,11 +1,14 @@
 from django.core.management.base import BaseCommand
-from myapp2.models import Order
+from myapp2.models import Product
 
 
 class Command(BaseCommand):
     help = "Create order."
 
     def handle(self, *args, **kwargs):
-        order = Order(customer='', products='', total_price=None)
-        order.save()
-        self.stdout.write(f'{order}')
+        bottle_of_water = Product(product_name='bottle_of_water',
+                                  price=2.5,
+                                  description="bottle_of_water",
+                                  image=None)
+        bottle_of_water.save()
+        self.stdout.write(f'{bottle_of_water}')
